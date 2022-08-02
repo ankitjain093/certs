25c5dd6d79dc517cfd4fa7fa074ff433a759c0eb
root <root@bng-hb-dev-18.juniper.net>
2022-08-02 14:47:37 +0530

    correct arg

--- certs/gen-client-certs.sh
+++ certs/gen-client-certs.sh
@@ -2,11 +2,11 @@
 # ./gen-cert.sh bng-lic-jift-01
 
 # Generate valid Client Key/Cert
-openssl genrsa -passout pass:1234 -des3 -out client-$2.key 4096
-openssl req -passin pass:1234 -new -key client-$2.key -out client-$2.csr -subj "/C=SP/ST=Spain/L=Valdepenias/O=Test/OU=Client/CN=$2"
-openssl x509 -passin pass:1234 -req -days 365 -in client-$2.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out client-$2.crt
+openssl genrsa -passout pass:1234 -des3 -out client-$1.key 4096
+openssl req -passin pass:1234 -new -key client-$1.key -out client-$1.csr -subj "/C=SP/ST=Spain/L=Valdepenias/O=Test/OU=Client/CN=$1"
+openssl x509 -passin pass:1234 -req -days 365 -in client-$1.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out client-$1.crt
 
 # Remove passphrase from Client Key
-openssl rsa -passin pass:1234 -in client-$2.key -out client-$2.key
+openssl rsa -passin pass:1234 -in client-$1.key -out client-$1.key
 
-cat client-$2.crt client-$2.key > client-$2.pem
+cat client-$1.crt client-$1.key > client-$1.pem
